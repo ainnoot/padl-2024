47d9002447f4cf568d9153d4422b886ce57ceb41
ainnoot <ai6chr+github@gmail.com>
2023-09-18 08:53:40 +0200

    read_xes must be return a legacy log object to iterate over traces

--- xes2lp.py
+++ xes2lp.py
@@ -1,22 +1,24 @@
+import sys
 import pm4py
 
 
 def xes2lp(log_file):
-    log = pm4py.read_xes(log_file)
-    nb_traces = len(log)
-    f = open(log_file[:-3]+'lp','w')
-    for i in range(nb_traces):
-        trace = log[i]
-        for j in range(len(trace)):
-            event = trace[j]
-            event_name = '"'+event['concept:name']+'"'
-            f.write('trace({},{},{}).\n'.format(i,j,event_name))
-        f.write('\n')
-    f.close() 
-    
-import sys 
+    log = pm4py.read_xes(log_file, return_legacy_log_object=True)
+    output_target = "{}.lp".format(log_file[:-4])
+
+    with open(output_target, 'w') as f:
+        for tid, trace in enumerate(log):
+            for t, event in enumerate(trace):
+                f.write('trace({},{},\"{}\").\n'.format(
+                    tid, t, event['concept:name']))
+            f.write('\n')
+
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} [XES event log]".format(__file__))
+        sys.exit(1)
+
     log_file = sys.argv[1]
     xes2lp(log_file)
-    
+    sys.exit(0)
