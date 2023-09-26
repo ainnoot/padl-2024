d7ecdfbacdb579c17235a73b64a4ed6bd28819c6
ainnoot <ai6chr+github@gmail.com>
2023-09-26 10:56:28 +0200

    Script to process a whole folder

--- xes2lp.py
+++ xes2lp.py
@@ -1,6 +1,6 @@
 import sys
 import pm4py
-
+from pathlib import Path
 
 def xes2lp(log_file):
     log = pm4py.read_xes(log_file.absolute().as_posix(), return_legacy_log_object=True)
@@ -9,3 +9,16 @@ def xes2lp(log_file):
         for t, event in enumerate(trace):
             prg.append('trace({},{},\"{}\").'.format(tid, t, event['concept:name']))
     return '\n'.join(prg)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} [log folder]\n\nWhere [log folder] contains a subfolder `xes` with event logs in XES format. Reified logs are saved in [log folder].".format(__file__))
+        sys.exit(1)
+
+    log_folder = sys.argv[1]
+    for log in Path(log_folder).glob('xes/*.xes'):
+        log_prg = xes2lp(log)
+        with open(Path(log_folder).parent / (log.stem + '.lp'), 'w') as f:
+            f.write(log_prg)
+
+    sys.exit(0)
