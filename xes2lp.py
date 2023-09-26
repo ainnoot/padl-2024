b61cdb1365da5b250a12c8eccabff9841f928090
ainnoot <ai6chr+github@gmail.com>
2023-09-26 10:50:54 +0200

    trace(TID,A,T) -> trace(TID,T,A) log format

--- xes2lp.py
+++ xes2lp.py
@@ -3,22 +3,9 @@ import pm4py
 
 
 def xes2lp(log_file):
-    log = pm4py.read_xes(log_file, return_legacy_log_object=True)
-    output_target = "{}.lp".format(log_file[:-4])
-
-    with open(output_target, 'w') as f:
-        for tid, trace in enumerate(log):
-            for t, event in enumerate(trace):
-                f.write('trace({},{},\"{}\").\n'.format(
-                    tid, t, event['concept:name']))
-            f.write('\n')
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} [XES event log]".format(__file__))
-        sys.exit(1)
-
-    log_file = sys.argv[1]
-    xes2lp(log_file)
-    sys.exit(0)
+    log = pm4py.read_xes(log_file.absolute().as_posix(), return_legacy_log_object=True)
+    prg = []
+    for tid, trace in enumerate(log):
+        for t, event in enumerate(trace):
+            prg.append('trace({},{},\"{}\").'.format(tid, t, event['concept:name']))
+    return '\n'.join(prg)
