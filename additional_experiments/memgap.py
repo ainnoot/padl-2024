9ec2a4b2fcf5944068cde8c75cc8c17d7c0b6014
ainnoot <ai6chr+github@gmail.com>
2024-08-22 21:46:07 +0200

    memgap for d4py

--- additional_experiments/memgap.py
+++ additional_experiments/memgap.py
@@ -2,13 +2,16 @@ import sys
 import os
 from pyrunlim import pyrunlim_launch
 from memory_profiler import profile, memory_usage
-import clingo
+from time import perf_counter
 
 def run(model, log, method):
 	outputs = dict()
 
 	@profile
-	def main(model, log, method, outputs):
+	def main_asp(model, log, method, outputs):
+		import clingo
+		start = perf_counter()
+
 		ctl = clingo.Control()
 		ctl.load(model)
 		ctl.load(log)
@@ -20,7 +23,35 @@ def run(model, log, method):
 		outputs['execution_time'] = ctl.statistics['summary']['times']['total']
 		outputs['rules'] = ctl.statistics['problem']['lp']['rules']
 
-	mem_usage = memory_usage((main, (model, log, method, outputs), dict()), , interval=0.05)
+		end = perf_counter()
+		outputs['total_time'] = end - start
+
+	@profile
+	def main_d4py(model, log, method, outputs):
+		from Declare4Py.D4PyEventLog import D4PyEventLog
+		from Declare4Py.ProcessModels.DeclareModel import DeclareModel
+		from Declare4Py.ProcessMiningTasks.ConformanceChecking.MPDeclareAnalyzer import MPDeclareAnalyzer
+
+		# parse log
+		d4py_log = D4PyEventLog()
+		d4py_log.parse_xes_log(log)
+
+		print("Done parsing log...!")
+		start = perf_counter()
+
+		# parse decl
+		declare_model = DeclareModel()
+		declare_model.parse_from_file(model)
+
+		# do the conformance checking
+		checker = MPDeclareAnalyzer(log=d4py_log, declare_model=declare_model, consider_vacuity=True)
+		ans = checker.run()
+
+		end = perf_counter()
+		outputs['total_time'] = end - start
+
+
+	mem_usage = memory_usage((main_d4py if method == 'd4py' else main_asp, (model, log, method, outputs), dict()), interval=0.05)
 	outputs['memory_peak'] = max(mem_usage)
 	outputs['model'] = model
 	outputs['log'] = log
