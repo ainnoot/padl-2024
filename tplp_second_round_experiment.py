f4bd33818ab18d1341c1e3a7fcf1791b9cfb8dee
ainnoot <ai6chr+github@gmail.com>
2024-08-22 23:03:53 +0200

    better row format

--- tplp_second_round_experiment.py
+++ tplp_second_round_experiment.py
@@ -29,16 +29,28 @@ BASE_PATH = "additional_experiments"
 LOG_PATH = os.path.join(BASE_PATH, "synthetic_logs")
 MODEL_PATH = os.path.join(BASE_PATH, "declare_models")
 
+def make_command(log, model, method):
+	return ["python3", "conformance_checking.py", log, model, "-m", M, "-o", "/dev/null"]
+
+
 rows = []
-for C, L, M in product(CONSTRAINTS, LENGTHS, METHODS):
+for C, L in product(CONSTRAINTS, LENGTHS):
 	log = f"{LOG_PATH}/{C}_{L}.xes"
 	model = f"{MODEL_PATH}/{C}.decl"
-	command = ["python3", "conformance_checking.py", log, model, "-m", M, "-o", "/dev/null"]
+	command = make_command(log, model, 'd4py')
 	p = pyrunlim_launch(command)
 
-	rows.append((C, L, M, p.real, p.real, p.max_memory))
+	d4py_real = p.real
+	d4py_memo = p.max_memory
 
+	command = make_command(log, model, 'asp_native')
+	p = pyrunlim_launch(command)
 
-df = pd.DataFrame(rows, columns=["constraint", "length", "method", "real", "time", "max_memory"])
+	asp_real = p.real
+	asp_memo = p.max_memory
+
+	rows.append((C, L, d4py_real, asp_real, d4py_memo, asp_memo))
+
+df = pd.DataFrame(rows, columns=["constraint", "length", "d4py_real", "asp_real", "d4py_memo", "asp_memo"])
 
 df.to_csv("results.csv", index=False, header=True)
